813a61c3d723999427b988e426300628d751212e
ben hart <Benjmhart@gmail.com>
2018-03-21 00:00:19 -0400

    completed front end auth flow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import Header from "./Header";
+import Landing from "./Landing";
+import * as actions from "../actions/index";
 
-const Header = () => <h2>header</h2>
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Route path='/' component={Landing} />
-                    <Route path="/dashboard" component={Dashboard} />
-                    <Route path="/surveynew" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    )
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
-export default App
+export default connect(null, actions)(App);
